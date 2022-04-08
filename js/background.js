04c627a1955fd08bd0b9a3270619c3be2050c559
kawanakaiku <80381779+kawanakaiku@users.noreply.github.com>
2022-04-09 03:17:05 +0900

    Add files via upload

--- js/background.js
+++ js/background.js
@@ -1,4 +1,14 @@
 var caches = {}
+
+const fetch_retry = async (url, options, n) => {
+	try {
+	  return await fetch(url, options)
+	} catch (err) {
+	  if (n === 1) throw err
+	  return await fetch_retry(url, options, n - 1)
+	}
+}
+
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
 	if (!message) {
 		sendResponse({
@@ -10,14 +20,26 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
 		if (caches[url] !== undefined) {
 			sendResponse(caches[url])
 		} else {
-			fetch(url, {
+			fetch_retry(url, {
 				'method': 'GET'
 			}).then(r => r.text()).then(t => {
-				caches[url] = t;
+				caches[url] = t
 				sendResponse(t)
 			})
 		}
 	}
 
-	return true;
-});
+	return
+})
+
+/*
+var return_cancel = details => {
+	return { cancel: true }
+}
+
+chrome.webRequest.onBeforeRequest.addListener(
+	return_cancel,
+	{urls: ['https://nvcomment.nicovideo.jp/legacy/api.json']},
+	["blocking"]
+)
+*/
--- js/block.js
+++ js/block.js
@@ -1,40 +1,179 @@
-const parser = new DOMParser();
-const url = window.location.href
+"use strict"
 
-function main() {
-	if (url.startsWith('https://www.nicovideo.jp/search/')) {
-		var videos = document.querySelectorAll('li.item[data-video-item]')
-	} else if (url.startsWith('https://www.nicovideo.jp/video_top')) {
-		var videos = document.querySelectorAll('div.VideoCard[data-video-item], div.NicoadVideoItem[data-nicoad-item]')
-	} else {
-		var videos = []
-	}
+function video() {
+	"use strict"
+	//console.log('running video')
+	chrome.storage.local.get({
+		video_userids: '45261915\n92490088\n123420362\n123523139\n123530845\n23408687\n118851573\n118622294\n940035',
+		video_usernames: '',
+		video_blacktags: ''
+	}, function(items) {
+		video_remove(items.video_userids.split('\n'), items.video_usernames.split('\n'), items.video_blacktags.split('\n'))
+	})
+}
 
-	videos.forEach(
-		video => {
-			const sm = video.getAttribute('data-video-id')
-			if (sm === null) { return true; }
-			if (!sm.startsWith('sm')) { return true; }
+function video_remove(userids, usernames, blacktags) {
+	// https://www.nicovideo.jp/video_top
+	// https://www.nicovideo.jp/ranking
+	// https://www.nicovideo.jp/search/voiceroid
+
+	"use strict"
+	// console.log(blacktags)
+	var elms = document.querySelectorAll('li.item[data-video-item][data-video-id], div.VideoCard[data-video-item][data-video-id], div.NicoadVideoItem[data-nicoad-item][data-video-id], div.NicoadVideoItem[data-nicoad-item][data-content-id], div.NC-VideoCard[data-video-thumbnail-comment-hover], li.nicoadVideoItem[data-nicoad-video-item]')
+	elms = Array.from(elms).filter(x => !x.hasAttribute('notremoving'))
+
+	elms.forEach(
+		elm => {
+			var sm = elm.getAttribute('data-video-id')
+			if (sm === null) {
+				sm = elm.getAttribute('data-content-id')
+			}
+			if (sm === null) {
+				sm = 'sm' + elm.querySelector('img.thumb[src]').getAttribute('src').split('/')[4]
+			}
+			if (sm === null) {
+				sm = basename(elm.querySelector('a[href]').getAttribute('href'))
+			}
+			//console.log(sm)
 
 			chrome.runtime.sendMessage({
 					contentScriptQuery: 'post',
 					endpoint: `https://ext.nicovideo.jp/api/getthumbinfo/${sm}`
 				},
 				(response) => {
-					const text = response;
+					var text = response
 					// console.log(text)
-					const dom = parser.parseFromString(text, 'text/xml')
-					const userid = dom.querySelector('user_id').textContent
-					const username = dom.querySelector('user_nickname').textContent
-					//console.log(userid, username);
-					if (['45261915', '92490088', '123420362', '123523139', '123530845', '23408687', '118851573'].includes(userid)) {
-						console.log(`removing ${sm} userid:${userid} username:${username}`)
-						video.remove()
+					var dom = parser.parseFromString(text, 'text/xml')
+					try {
+						var userid = ( dom.querySelector('user_id') || dom.querySelector('ch_id') ).textContent
+						var username = ( dom.querySelector('user_nickname') || dom.querySelector('ch_name') ).textContent
+						var tags = Array.from(dom.querySelector('tags').querySelectorAll('tag'), x => x.textContent)
+					} catch (e) {
+						// console.error( `error parsing https://ext.nicovideo.jp/api/getthumbinfo/${sm} テキスト:${text} エラー内容:${e}`)
+						return
+					}
+					//console.log(userid, username)
+					// console.log(tags)
+					//console.log(`${sm} userid:${userid} username:${username} tags:${tags}`)
+					if (userids.includes(userid) || wildcards(usernames, username) || (tags.find(tag => blacktags.includes(tag)) !== undefined)) {
+						console.log(`removing ${sm} userid:${userid} username:${username} tags:${tags}`)
+						elm.remove()
+					} else {
+						elm.setAttribute("notremoving", "")
 					}
 				}
 			)
 
 
-		})
+		}
+	)
 }
-setInterval(main, 1000)
+
+function rpg() {
+	"use strict"
+	//console.log('running rpg')
+	chrome.storage.local.get({
+		rpg_games: 'gm8549',
+		rpg_userids: '',
+		rpg_usernames: ''
+	}, function(items) {
+		rpg_remove(items.rpg_games.split('\n'), items.rpg_userids.split('\n'), items.rpg_usernames.split('\n'))
+	})
+}
+
+function rpg_remove(games, userids, usernames) {
+	// https://game.nicovideo.jp/atsumaru/
+	// https://game.nicovideo.jp/atsumaru/ranking
+
+	"use strict"
+	var elms = document.querySelectorAll('section.MatrixRankingMatrix__MatrixRankingGame, div.GameCard__Card')
+	elms = Array.from(elms).filter(x => !x.hasAttribute('notremoving'))
+
+	elms.forEach(
+		elm => {
+			var gm = basename(elm.querySelector('a[href^="/atsumaru/games/gm"]').getAttribute('href'))
+			var userid = basename(elm.querySelector('a[href^="/atsumaru/users/"]').getAttribute('href'))
+			var username = elm.querySelector('a[href^="/atsumaru/users/"]').textContent
+
+			// console.log(gm , userid, username)
+			if (games.includes(gm) || userids.includes(userid) || wildcards(usernames, username)) {
+				console.log(`removing ${gm} userid:${userid} username:${username}`)
+				elm.remove()
+			} else {
+				elm.setAttribute("notremoving", "")
+			}
+		}
+
+	)
+}
+
+function wildcard(pattern, string) {
+	// *, ? , \ に対応
+	// \　はエスケープ
+
+	"use strict"
+	var i = 0
+	var any = false
+	var index, w
+	while (i < pattern.length) {
+		w = pattern[i]
+		switch (w) {
+			case '*':
+				any = true
+				break
+			case '?':
+				if (string[0] !== undefined) {
+					string = string.slice(1)
+				} else {
+					return false
+				}
+				any = false
+				break
+			case '\\':
+				w = pattern[++i]
+			default:
+				while (i + 1 < pattern.length && !['*', '?'].includes(pattern[i + 1])) {
+					w += pattern[++i]
+				}
+				index = string.indexOf(w)
+				if (index === -1 || (!any && index !== 0)) {
+					return false
+				}
+				string = string.slice(index + w.length)
+				any = false
+		}
+		i++
+	}
+	if (!any && string !== '') {
+		return false
+	}
+	return true
+}
+
+function wildcards(patterns, string) {
+	"use strict"
+	return (patterns.find(pattern => wildcard(pattern, string)) !== undefined)
+}
+
+const parser = new DOMParser()
+const url = window.location.href
+const basename = (url) => url.split(/[\\/]/).pop()
+
+function main() {
+	if (wildcard('https://www.nicovideo.jp/*', url)) {
+		setInterval(video, 400)
+	} else if (wildcard('https://game.nicovideo.jp/*', url)) {
+		setInterval(rpg, 400)
+	}
+}
+
+// check if filtering enabled
+chrome.storage.local.get({
+	filter_enabled: true
+}, function(items) {
+	if (items.filter_enabled) {
+		main()
+	} else {
+		console.log('Niconico filter has been disabled.')
+	}
+})
